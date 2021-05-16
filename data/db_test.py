45370ae6674dedacbd2b057253b0c96d15cc5b65
Jacinto Mba Cantero <jacmba@gmail.com>
2021-05-16 22:21:47 +0100

    Working unit tests

--- data/db_test.py
+++ data/db_test.py
@@ -1,18 +1,17 @@
-from data.db import DB
-import pytest
+import unittest
 from unittest.mock import patch
+from data.db import DB
 
 
-@pytest.fixture
-def db():
-    return DB("mongo://mock")
+class test_db(unittest.TestCase):
+    def setUp(self):
+        self.db = DB("mongodb://mock")
 
+    def test_object_creation(self):
+        assert self.db.uri == "mongodb://mock"
 
-def test_db_object(db):
-    assert db.uri == "mongo://mock"
-
-
-@patch("pymongo.MongoClient")
-def test_client_connection(db):
-    db.connect()
-    assert db.client["foo"] == "bar"
+    @patch("data.db.pymongo.MongoClient", return_value={"foo": "bar"})
+    def test_client_connection(self, mock_MongoClient):
+        self.db.connect()
+        mock_MongoClient.assert_called_once_with("mongodb://mock")
+        assert self.db.client["foo"] == "bar"
