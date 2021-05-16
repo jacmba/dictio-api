b6e1cd56e3c7806ee93b7fcbba8c26a302d32423
Jacinto Mba Cantero <jacmba@gmail.com>
2021-05-16 22:48:05 +0100

    :construction: find collections test

--- data/db.py
+++ data/db.py
@@ -2,8 +2,13 @@ import pymongo
 
 
 class DB:
-    def __init__(self, uri) -> None:
+    def __init__(self, uri, database) -> None:
         self.uri = uri
+        self.database = database
 
     def connect(self):
         self.client = pymongo.MongoClient(self.uri)
+
+    def find_alphabet(self):
+        cols = self.client[self.database].list_collection_names()
+        return cols
--- data/db_test.py
+++ data/db_test.py
@@ -5,13 +5,25 @@ from data.db import DB
 
 class test_db(unittest.TestCase):
     def setUp(self):
-        self.db = DB("mongodb://mock")
+        self.db = DB("mongodb://mock", "mock")
 
     def test_object_creation(self):
         assert self.db.uri == "mongodb://mock"
+        assert self.db.database == "mock"
 
     @patch("data.db.pymongo.MongoClient", return_value={"foo": "bar"})
-    def test_client_connection(self, mock_MongoClient):
+    def test_client_connection(self, mock):
         self.db.connect()
-        mock_MongoClient.assert_called_once_with("mongodb://mock")
+        mock.assert_called_once_with("mongodb://mock")
         assert self.db.client["foo"] == "bar"
+
+    @patch("data.db.pymongo.MongoClient",
+           return_value={
+               "mock": {
+                   "list_collection_names": lambda: ["A", "B", "C"]
+               }
+           })
+    def test_find_alphabet(self, mock):
+        self.db.connect()
+        result = self.db.find_alphabet()
+        assert result == ["A", "B", "C"]
